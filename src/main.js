3fa5635eff11a3aed34d0a8a9c4386820235a82a
Mariela Torres <martz_maru@hotmail.com>
2019-07-12 21:19:16 -0500

    Subiendo versión revisada en casa de D

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 const allPokemon = document.getElementById("all-Pokemon");  //Esta es la sección donde se van a pintar las tarjetas
 let type = document.getElementById("type"); //Esta es la lista deplegable con los tipos de pokémon
 let order = document.getElementById("order"); // Lista despeglable para ordenar los pokemones
-const dataPokemon = POKEMON.pokemon; //Data
+const dataPokemon = window.POKEMON.pokemon; //Data
+
 let pokearray = [];
 let pokearrayOrganized = [];
-let filtered = [];
+
+const inicio = document.getElementById("section-one");
+const introduction = document.getElementById("home-page");
+const pokedex = document.getElementById("pokedex");
+const pokeGraph = document.getElementById("graph");
+const pokemones = document.getElementById("section-pokemon");
+const sectionGraph = document.getElementById("show-graph");
 
 const mostrarData = (data) => {
     let str =  '';
@@ -32,34 +39,116 @@ const mostrarData = (data) => {
                             <p> Weaknesses: </p>
                             <p>${element.weaknesses}</p>
                             <p> Prev. Evolution: </p>
-                            <p>${element.prev_evolution}</p>
+                            <p> ${element.prev_evolution}</p>
                             <p> Next Evolution: </p>
-                            <p>${element.next_evolution}</p>
+                            <p> ${element.next_evolution} </p>
                         </div>
                    </div>
                </div>
-           </div>`                   
+           </div>`  ;                      
     });
 allPokemon.innerHTML = str;
 };
 mostrarData(dataPokemon);
 
-
 //Función "showType"
-
-showType = event => {
+const showType = event => {
 const condition = event.target.value; //Guardando el tipo de Pokémon que elige el usuario
 pokearray = window.filterData(dataPokemon,condition); //Se guarda como nuevo arreglo, el resultado de la función filterData
 mostrarData(pokearray);
 };
 
 //Funcion "orderPokemon"
-orderPokemon = event => {
+const orderPokemon = event => {
+    if (pokearray.length == "0"){
+        pokearray= dataPokemon;
+    }
     const criterion = event.target.value; //Guardando el orden que elige el usuario
     pokearrayOrganized = window.sortData(pokearray,criterion); //Se guarda como nuevo arreglo, el resultado de la función filterData
     mostrarData(pokearrayOrganized);
 };
 
- type.addEventListener("change", showType); //La lista desplegable responderá a un cambio y ejecuta la función "showtype"
- order.addEventListener("change",orderPokemon);
+
+//estadisticas
+const typeP = ["Water", "Fire", "Grass", "Ground", "Ice", "Electric", "Rock", "Flying", "Poison", "Bug", "Psychic", "Normal", "Fighting", "Dragon"];
+let numType = [];
+typeP.forEach(element =>{
+   numType.push((window.filterData(dataPokemon,element).length));
+})
+console.log(numType);
+
+let porcentageTypePokemon = [];
+
+numType.forEach(element => {
+   porcentageTypePokemon.push(Math.round((element/dataPokemon.length)*100) + '%')
+})
+console.log(porcentageTypePokemon)
+
+//grafica
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(graphType);
+
+function graphType (){
+   let data = google.visualization.arrayToDataTable([
+       ['Tipo', '%'],
+       ['Agua', 21],
+       ['Fuego', 8],
+       ['Planta', 9],
+       ['Tierra', 9],
+       ['Hielo', 3],
+       ['Eléctrico', 6],
+       ['Roca', 7],
+       ['Volador', 13],
+       ['Venenoso', 22],
+       ['Insecto', 8],
+       ['Psíquico', 9],
+       ['Normal', 16],
+       ['Volador', 5],
+       ['Dragón', 2]
+   ]);
+
+   const options ={
+       title: 'Tipos de Pokemones',
+       is3D: true,
+   };
+
+   let chart = new google.visualization.PieChart(document.getElementById('div-show-graph'));
+   chart.draw(data,options);
+}
+
+const show = () =>{
+    pokemones.classList.remove("hidden");
+    inicio.classList.add("hidden");
+    sectionGraph.classList.add("hidden");
+ };
  
+ const showHome = () =>{
+    inicio.classList.remove("hidden");
+    pokemones.classList.add("hidden");
+    sectionGraph.classList.add("hidden");
+ };
+ 
+ const showGraph = () =>{
+    sectionGraph.classList.remove("hidden");
+    pokemones.classList.add("hidden");
+    inicio.classList.add("hidden");
+ }
+
+type.addEventListener("change", showType); //La lista desplegable responderá a un cambio y ejecuta la función "showtype"
+order.addEventListener("change",orderPokemon);
+pokedex.addEventListener("click",show);
+introduction.addEventListener("click", showHome);
+pokeGraph.addEventListener("click", showGraph);
+ 
+//  let prevolution=document.getElementById("prevolution"); //Borrar si no es posible mostrar evoluciones
+ 
+//  If ([element].prev_evolution[0] == "undefined"){
+//      prevolution.innerHTML="NONE";
+// } else {
+//      prevolution.innerHTML= [element].prev_evolution[0].name:
+//   };
+//  If([element].prev_evolution[0] == "undefined") {
+//     evolution.innerHTML="NONE";
+// } else {
+//     evolution.innerHTML= [element].prev_evolution[0].name:
+//  };
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,51 @@
+// require('../src/data.js');
+
+
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
+
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
+
 require('../src/data.js');
+require('../src/data/pokemon/pokemon.js');
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+//Testeando filterData
+describe("filterData",()=>{
+  it("is a function",() =>{
+    expect(typeof filterData).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it("returns `There are 3 pokemon type dragon`",()=>{
+    expect(window.filterData(window.POKEMON.pokemon,"Dragon").length).toBe(3);
+  });
+});
+
+//Testeando orderData Alfabético 
+describe("sortData",()=>{
+  it("is a function",() =>{
+    expect(typeof sortData).toBe("function");
+  });
+
+  it("returns `Descending alphabetical order: Should return Abra, Aerodactyl, Alakazam, Wigglytuff Zapdos and Zubat for positions 0,1,2,148,149 and 150`",()=>{
+    expect(window.sortData(window.POKEMON.pokemon,"A-to-Z")[0].name).toBe("Abra");
+    expect(window.sortData(window.POKEMON.pokemon,"A-to-Z")[1].name).toBe("Aerodactyl");
+    expect(window.sortData(window.POKEMON.pokemon,"A-to-Z")[2].name).toBe("Alakazam");
+    expect(window.sortData(window.POKEMON.pokemon,"A-to-Z")[148].name).toBe("Wigglytuff");
+    expect(window.sortData(window.POKEMON.pokemon,"A-to-Z")[149].name).toBe("Zapdos");
+    expect(window.sortData(window.POKEMON.pokemon,"A-to-Z")[150].name).toBe("Zubat");
+  });
+
+  it("returns `Descending numerical order: Should return Mew, Mewtwo, Dragonite, Venasaur, Ivysaur and Bulbasaur for positions 0,1,2,148,149 and 150`",()=>{
+    expect(window.sortData(window.POKEMON.pokemon,"9-to-0")[0].name).toBe("Mew");
+    expect(window.sortData(window.POKEMON.pokemon,"9-to-0")[1].name).toBe("Mewtwo");
+    expect(window.sortData(window.POKEMON.pokemon,"9-to-0")[2].name).toBe("Dragonite");
+    expect(window.sortData(window.POKEMON.pokemon,"9-to-0")[148].name).toBe("Venusaur");
+    expect(window.sortData(window.POKEMON.pokemon,"9-to-0")[149].name).toBe("Ivysaur");
+    expect(window.sortData(window.POKEMON.pokemon,"9-to-0")[150].name).toBe("Bulbasaur");
   });
 });
